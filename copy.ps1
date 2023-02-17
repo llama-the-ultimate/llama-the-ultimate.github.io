b9731b5697e96c736a448f1ee30f8cb8929a4807
Jonas <jonas.winje@gmail.com>
2023-02-17 22:38:36 +0100

    Update copy.ps1

--- copy.ps1
+++ copy.ps1
@@ -1,28 +1,13 @@
-$stuff = ConvertFrom-Json((Invoke-WebRequest "https://llama-the-ultimate.glitch.me/stuff").Content);
-foreach  ($file in $stuff.assets) {
-    Invoke-WebRequest  "https://llama-the-ultimate.glitch.me/assets/$file" -OutFile (New-Item -Type File -Path "./assets/$file" -Force);
-    }
-foreach  ($note in $stuff.notes) {
-    $noext = $note.file.substring(0, $note.file.length - 4);
-    Invoke-WebRequest "https://llama-the-ultimate.glitch.me/notes/$noext.html" -OutFile (New-Item -Type File -Path "./notes/$noext.html" -Force);
-    Invoke-WebRequest "https://llama-the-ultimate.glitch.me/gd/notes/$noext.txt" -OutFile (New-Item -Type File -Path "./gd/notes/$noext.txt" -Force);
+$urls = ConvertFrom-Json((Invoke-WebRequest "https://llama-the-ultimate.glitch.me/rpc/links?url=https%3A%2F%2Fllama-the-ultimate.org").Content);
+foreach  ($url in $urls) {
+  $path = ".$(([uri]$url).AbsolutePath)";
+  Invoke-WebRequest $url -OutFile (New-Item -Type File -Path $path -Force);
 }
-foreach ($otherFile in $stuff.other) {
-    Invoke-WebRequest "https://llama-the-ultimate.glitch.me/$otherFile" -OutFile (New-Item -Type File -Path "./$otherFile" -Force );
-}
-Invoke-WebRequest "https://llama-the-ultimate.glitch.me/feed.xml?url=https%3A%2F%2Fllama-the-ultimate.org" -OutFile (New-Item -Type File -Path "./feed.xml" -Force );
 
-$stuffFiles = $stuff.notes | % { $_.file }
-
-<#
 foreach ($file in Get-ChildItem "./gd/notes/") {
-    if (!($stuffFiles -contains $file)) {
-        $fname = $file.name;
-        $noext = $fname.substring(0, $fname.length - 4);
-        rm "./gd/notes/$noext.txt";
-        if (Test-Path "./notes/$noext.html") {
-            rm "./notes/$noext.html";
-        }
+    if (($urls -contains "https://llama-the-ultimate.glitch.me/gd/notes/$($file.Name)")) {
+        Write-Host "notes/$($file.BaseName) should stay";
+    } else {
+        Write-Host "notes/$($file.BaseName) should go";
     }
 }
-#>
